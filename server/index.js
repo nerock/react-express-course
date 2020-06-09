6ce2340e9b32996a9276c6eeb4c848187e3988ea
nerock <hi@nerock.dev>
2020-06-09 18:37:05 +0200

    Initialize first users model before using it

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { mongoURI } = require("./config/keys");
-require("./services/passport");
 require("./models/User");
+require("./services/passport");
 
 const app = express();
 
