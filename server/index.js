6115da62d5617229a26bfbdd7c520c25c5c6ddce
nerock <hi@nerock.dev>
2020-06-09 18:09:47 +0200

    Create a user schema with mongoose

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { mongoURI } = require("./config/keys");
 require("./services/passport");
+require("./models/User");
 
 const app = express();
 
 require("./routes/authRoutes")(app);
+mongoose.connect(mongoURI);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
