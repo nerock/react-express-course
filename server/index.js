1cbfcc25e50c5374e0de3973ad5d2a5939d02db4
nerock <hi@nerock.dev>
2020-06-10 18:57:32 +0200

    Add cookie library

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const { mongoURI } = require("./config/keys");
 require("./models/User");
 require("./services/passport");
--- server/services/passport.js
+++ server/services/passport.js
@@ -22,7 +22,7 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accesToken, refreshToken, profile, done) => {
+    (_, _, profile, done) => {
       User.findOne({ googleID: profile.id }).then((existingUser) => {
         if (existingUser) {
           done(null, existingUser);
