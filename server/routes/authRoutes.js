8663ffa6bdecd8b4f98474d6427cc083993f2bcc
nerock <hi@nerock.dev>
2020-06-10 19:25:51 +0200

    Add logout endpoint and remove test user endpoint

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,7 +10,8 @@ module.exports = (app) => {
 
   app.get("/auth/google/callback", passport.authenticate("google"));
 
-  app.get("/api/user", (req, res) => {
+  app.get("/auth/logout", (req, res) => {
+    req.logout();
     res.send(req.user);
   });
 };
