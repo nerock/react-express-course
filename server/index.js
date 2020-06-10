569121af43b246abc68db7942ba63d7b6a8b3241
nerock <hi@nerock.dev>
2020-06-10 19:00:05 +0200

    Configure express to use cookie session

--- server/index.js
+++ server/index.js
@@ -2,14 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const { mongoURI } = require("./config/keys");
+const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 Days
+    keys: [keys.cookieKey],
+  })
+);
+
 require("./routes/authRoutes")(app);
-mongoose.connect(mongoURI);
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
