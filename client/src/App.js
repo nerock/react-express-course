981a18962eb0bdfc2a4697a61dadf59ed5843195
nerock <hi@nerock.dev>
2020-06-11 15:37:32 +0200

    Add frontend files

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Manuel "nerock" Adalid Moya</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a href="/auth/google">Sign In With Google</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
