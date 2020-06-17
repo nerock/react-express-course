4ee8ceb8834545b785c8f7e2db74401844d32d38
nerock <hi@nerock.dev>
2020-06-17 21:36:35 +0200

    Create the charge with stripe api

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,5 +1,15 @@
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
+
 module.exports = (app) => {
-  app.post("/api/stripe", (req, res) => {
-    console.log(req);
+  app.post("/api/stripe", async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "eur",
+      description: "5 credits",
+      source: req.body.id,
+    });
+
+    res.send(charge);
   });
 };
