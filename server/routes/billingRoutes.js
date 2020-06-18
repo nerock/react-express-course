273711febc3ad10c307726a6a6a8dfccbec35a54
nerock <hi@nerock.dev>
2020-06-18 22:47:13 +0200

    Move stripe charge call to another method, increment user credits and return the updated user

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,13 +3,20 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = (app) => {
   app.post("/api/stripe", async (req, res) => {
-    const charge = await stripe.charges.create({
-      amount: 500,
-      currency: "eur",
-      description: "5 credits",
-      source: req.body.id,
-    });
+    charge(req.body.id);
 
-    res.send(charge);
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
+  });
+};
+
+const charge = async (token) => {
+  return await stripe.charges.create({
+    amount: 500,
+    currency: "eur",
+    description: "5 credits",
+    source: token,
   });
 };
