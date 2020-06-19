56d5e342d4518c64a3e35e8e16bbc7be5f384ed9
nerock <hi@nerock.dev>
2020-06-20 01:08:45 +0200

    Add some margin

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,7 +20,9 @@ class Header extends Component {
             <Payments />
           </li>,
 
-          <li key="2">Credits: {this.props.auth.credits}</li>,
+          <li key="2" style={{ margin: "0 10px" }}>
+            Credits: {this.props.auth.credits}
+          </li>,
 
           <li key="3">
             <a href="/auth/logout">Log out</a>
