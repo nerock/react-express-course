86507f1fd491b04ac9484383dfdfee5fb9c8471f
nerock <hi@nerock.dev>
2020-06-21 00:58:40 +0200

    Create survey object

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,24 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 
+const Survey = mongoose.model("surveys");
+
+const generateRecipients = (recipients) => {
+  return recipients.split(",").map((email) => ({ email }));
+};
+
 module.exports = (app) => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {});
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: generateRecipients(recipients),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+  });
 };
