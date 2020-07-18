0822967aac5d2a2bca15578368b65dfd3247408a
nerock <hi@nerock.dev>
2020-07-18 17:05:06 +0200

    Add a thanks page after voting

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -11,6 +11,10 @@ const generateRecipients = (recipients) => {
 };
 
 module.exports = (app) => {
+  app.get("/api/surveys/thanks", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
